f841558829ccfc6237e69d00f90f2dfe00aeacea
Montplusa <toomynakai@gmail.com>
2022-11-25 16:40:36 +0900

    ホームページを追加する

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ from app.app import app
 from waitress import serve
 
 
+@app.route("/", methods=["GET"])
+def home():
+    return "ホームページ（仮）"
+
+
 if __name__ == "__main__":
 
     serve(app, host="0.0.0.0", port=5000)
