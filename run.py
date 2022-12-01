cce5a1057057f327535b103982d68fedb7ffc7b7
Montplusa <toomynakai@gmail.com>
2022-12-01 13:50:20 +0900

    homeへのリダイレクトを追加する

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 from app.app import app
 from waitress import serve
+import flask
 
 
 @app.route("/home", methods=["GET"])
 def home():
     return "ホームページ（仮）"
+
+
+@app.route("/", methods=["GET"])
+def root():
+    # return flask.redirect("http://localhost:5000/home")でもOK
+    return flask.redirect(flask.url_for("home"))  # この"home"は関数名
 
 
 if __name__ == "__main__":
